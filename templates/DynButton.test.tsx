9e22c0a3e924a701ded1f726c1adca349b67348e
m.gasic <m.gasic@LTC0IT244>
2025-10-18 04:37:08 +0200

    components finaliyation

--- templates/DynButton.test.tsx
+++ templates/DynButton.test.tsx
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react';
 import * as React from 'react';
-import { DynButton } from './DynButton';
+import { DynButton } from '../packages/dyn-ui-react/src/components/DynButton/DynButton';
 describe('DynButton', () => {
   it('exports a React component', () => { expect(typeof DynButton).toBe('function'); });
   it('renders without crashing', () => { render(<DynButton>Click</DynButton>); });
