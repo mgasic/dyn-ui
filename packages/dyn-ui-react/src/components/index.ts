c075eeed55f9a4c8f223ecfdc7e840a7e8917723
mgasic <48404494+mgasic@users.noreply.github.com>
2025-10-21 09:00:48 +0200

    chore: regenerate component exports index

--- packages/dyn-ui-react/src/components/index.ts
+++ packages/dyn-ui-react/src/components/index.ts
@@ -1,6 +1,6 @@
 ﻿// Standardized exports for ALL DYN-UI components
 // Ovaj fajl pokriva sve komponente - ne menjaj ručno!
-// Generated on: 2025-10-14 17:37:11
+// Generated on: 2025-10-21 06:43:00
 export { DynAvatar } from './DynAvatar';
 export { DynBadge } from './DynBadge';
 export { DynBox } from './DynBox';
